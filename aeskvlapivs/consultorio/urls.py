32ab7872f2ed0640c1021dc08a9de00b0cfa02e8
Juan Víctor <juanvictoremilio@icloud.com>
2022-06-28 13:53:00 -0600

    CBV Create

--- aeskvlapivs/consultorio/urls.py
+++ aeskvlapivs/consultorio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from consultorio.views import PacienteListView, PacienteDetailView
+from consultorio.views import PacienteListView, PacienteDetailView, PacienteCreate
 
 urlpatterns = [
     path('', PacienteListView.as_view(), name='pacientes'),
-    path('<int:pk>/<slug:page_slug>/', PacienteDetailView.as_view(), name='paciente'),
+    path('<int:pk>/<slug:paciente_slug>/', PacienteDetailView.as_view(), name='paciente'),
+    path('create/', views.PacienteCreate.as_view(),name='nuevo_paciente')
 ]
 
 
--- aeskvlapivs/consultorio/views.py
+++ aeskvlapivs/consultorio/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse, reverse_lazy
 from .models import Paciente
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Paciente
 
@@ -10,6 +11,12 @@ class PacienteListView(ListView):
 
 class PacienteDetailView(DetailView):
     model = Paciente
+
+class PacienteCreate(CreateView):
+    model = Paciente
+    fields = ['name','dob', 'age', 'phone', 'immediate_background']
+    
+    success_url = reverse_lazy('pacientes')
 
 
 # Create your views here.
